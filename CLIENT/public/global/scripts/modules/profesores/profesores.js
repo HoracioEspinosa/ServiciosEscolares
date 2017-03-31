9ad29d3757cf99a17d4276924642f38ada980a67
Lic. José Ángel Peña Godínez <licangelgodinez@hotmail.com>
2017-03-31 01:49:33 -0600

    Cambio de campo telefono por phone en BD

--- CLIENT/public/global/scripts/modules/profesores/profesores.js
+++ CLIENT/public/global/scripts/modules/profesores/profesores.js
@@ -8,7 +8,7 @@ $(document).on('click', '.mybutton', function(e) {
         $(e.currentTarget).find('input[name="modmlname"]').val(apellido[1]);
         $(e.currentTarget).find('input[name="modcedula"]').val(data[0].cedulas);
         $(e.currentTarget).find('input[name="modemail"]').val(data[0].email);
-        $(e.currentTarget).find('input[name="modphone"]').val(data[0].telefono);
+        $(e.currentTarget).find('input[name="modphone"]').val(data[0].phone);
         $(e.currentTarget).find('textarea[name="modnotes"]').val(data[0].notas);
         $(e.currentTarget).find('input[name="modestatus"]').val(data[0].estatus);
         $(e.currentTarget).find('input[name="idInformacion"]').val(data[0].idInformacion);
@@ -18,7 +18,7 @@ $(document).on('click', '.mybutton', function(e) {
         $(e.currentTarget).find('p[name="panombre"]').text(data[0].nombre + " " + apellido[0] + " " +apellido[1]);
         $(e.currentTarget).find('p[name="pacedula"]').text(data[0].cedulas);
         $(e.currentTarget).find('p[name="paemail"]').text(data[0].email);
-        $(e.currentTarget).find('p[name="patelefono"]').text(data[0].telefono);
+        $(e.currentTarget).find('p[name="patelefono"]').text(data[0].phone);
         $(e.currentTarget).find('p[name="panotas"]').text(data[0].notas);
         $(e.currentTarget).find('p[name="paestatus"]').text(data[0].estatus);
     });
